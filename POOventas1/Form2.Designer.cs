5465d9ebb9b0e4798d83637b57ddb2393599c48b
Majo-2306 <maria_castro24@unikino.edu.mx>
2025-03-03 23:36:25 -0700

    Agregar archivos de proyecto.

---
+++ POOventas1/Form2.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace POOventas1
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            rbContado = new RadioButton();
+            rbCredito = new RadioButton();
+            btnCalcular_Click = new Button();
+            dataGridView1 = new DataGridView();
+            txtMonto = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // rbContado
+            // 
+            rbContado.AutoSize = true;
+            rbContado.Font = new Font("Tahoma", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            rbContado.Location = new Point(301, 105);
+            rbContado.Name = "rbContado";
+            rbContado.Size = new Size(97, 26);
+            rbContado.TabIndex = 0;
+            rbContado.TabStop = true;
+            rbContado.Text = "Contado";
+            rbContado.UseVisualStyleBackColor = true;
+            // 
+            // rbCredito
+            // 
+            rbCredito.AutoSize = true;
+            rbCredito.Font = new Font("Tahoma", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            rbCredito.Location = new Point(412, 105);
+            rbCredito.Name = "rbCredito";
+            rbCredito.Size = new Size(87, 26);
+            rbCredito.TabIndex = 0;
+            rbCredito.TabStop = true;
+            rbCredito.Text = "Credito";
+            rbCredito.UseVisualStyleBackColor = true;
+            // 
+            // btnCalcular_Click
+            // 
+            btnCalcular_Click.Font = new Font("Tahoma", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnCalcular_Click.Location = new Point(12, 319);
+            btnCalcular_Click.Name = "btnCalcular_Click";
+            btnCalcular_Click.Size = new Size(231, 39);
+            btnCalcular_Click.TabIndex = 1;
+            btnCalcular_Click.Text = "Registrar Compra";
+            btnCalcular_Click.UseVisualStyleBackColor = true;
+            btnCalcular_Click.Click += btnCalcular_Click_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 135);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(487, 178);
+            dataGridView1.TabIndex = 2;
+            // 
+            // txtMonto
+            // 
+            txtMonto.Location = new Point(12, 102);
+            txtMonto.Name = "txtMonto";
+            txtMonto.PlaceholderText = "Monto";
+            txtMonto.Size = new Size(165, 27);
+            txtMonto.TabIndex = 3;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.coppel;
+            ClientSize = new Size(634, 390);
+            Controls.Add(txtMonto);
+            Controls.Add(dataGridView1);
+            Controls.Add(btnCalcular_Click);
+            Controls.Add(rbCredito);
+            Controls.Add(rbContado);
+            Name = "Form2";
+            Text = "Form2";
+            Load += Form2_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private RadioButton rbContado;
+        private RadioButton rbCredito;
+        private Button btnCalcular_Click;
+        private DataGridView dataGridView1;
+        private TextBox txtMonto;
+    }
+}
---
+++ POOventas1/Form2.cs
@@ -0,0 +1,84 @@
+﻿using POOventas1;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POOventas1
+{
+    public partial class Form2 : Form //Herencia
+    {
+        private List<Venta> ventas = new List<Venta>();//Este es un encapsulamiento de nuestra lista de ventas
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e) //este ejecuta el formulario
+        {
+
+        }
+
+        private void btnCalcular_Click_Click(object sender, EventArgs e)
+        {
+            double monto = double.Parse(txtMonto.Text); //Este nos convierte el texto a número
+            Venta venta; //Polimorfismo
+
+            if (rbContado.Checked)
+                venta = new VentaContado(monto); //De venta se crea una nueva instacia que es new VentaContado y lo mismo pasa con new VentaCredito
+            else
+                venta = new VentaCredito(monto);
+
+            ventas.Add(venta); //Agregar ventas
+            ActualizarGrid(); 
+        }
+        private void ActualizarGrid()
+        {
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = ventas.ConvertAll(v => new { Tipo = v.GetType().Name, Monto = v.Monto, Total = v.CalcularTotal() }); //Metodo de manipulación, comvierte los datos capturados a un formato legible.
+        }
+        public abstract class Venta //Clase Abstracta 
+        {
+            protected double monto; // Encapsulamiento
+
+            public Venta(double monto)
+            {
+                this.monto = monto;
+            }
+
+            public double Monto { get { return monto; } } //Encapsulamiento
+
+            public abstract double CalcularTotal(); // Metodo abstracto CalcularTotal
+        }
+
+        public class VentaContado : Venta //Herencia 
+        {
+            public VentaContado(double monto) : base(monto) { }
+
+            public override double CalcularTotal()
+            {
+                return monto;
+            }
+        }
+
+        public class VentaCredito : Venta //Herencia
+        {
+            public VentaCredito(double monto) : base(monto) { }
+
+            public override double CalcularTotal()
+            {
+                return monto * 1.05; // Incremento del 5% en ventas a credito
+            }
+        }
+    }
+}
+
+
+   
+
